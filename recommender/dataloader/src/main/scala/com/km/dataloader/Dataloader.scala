c5924003a26143c905d592a7e0e09778629c155f
km <313976009@qq.com>
2018-12-20 15:25:55 +0800

    电影数据已经上传至elasticsearch，并且电影数据新增加了Tags属性，属性值为用户对电影的标签，多个标签中间用 | 进行分隔

--- recommender/dataloader/src/main/scala/com/km/dataloader/Dataloader.scala
+++ recommender/dataloader/src/main/scala/com/km/dataloader/Dataloader.scala
@@ -1,9 +1,18 @@
 package com.km.dataloader
 
+import java.net.InetAddress
+
 import com.mongodb.casbah.commons.MongoDBObject
 import com.mongodb.casbah.{MongoClient, MongoClientURI}
 import org.apache.spark.SparkConf
 import org.apache.spark.sql.{DataFrame, SparkSession}
+import org.elasticsearch.action.admin.indices.create.CreateIndexRequest
+import org.elasticsearch.action.admin.indices.delete.DeleteIndexRequest
+import org.elasticsearch.action.admin.indices.exists.indices.IndicesExistsRequest
+import org.elasticsearch.common.settings.Settings
+import org.elasticsearch.common.transport.InetSocketTransportAddress
+import org.elasticsearch.transport.client.PreBuiltTransportClient
+import sun.net.InetAddressCachePolicy
 
 //movie数据集通过^ 分隔符
 //151^                                 电影ID
@@ -16,6 +25,7 @@ import org.apache.spark.sql.{DataFrame, SparkSession}
 // Action|Drama|Romance|War ^          电影类型
 // Liam Neeson|Jess|m Neeson           电影演员
 // Caton-Jones                         电影导演
+
 
 //Ratings数据集 用户对电影的评分
 //1,                     用户ID
@@ -63,8 +73,8 @@ object Dataloader {
   val MONGODN_RATING_COLLECTION="Rating"
   val MONGODN_TAG_COLLECTION="Tag"
 
-  val ES_MOVIE_INDEX="Movice"
-  val ES_RATING_INDEX="Ratting"
+  val ES_MOVIE_INDEX="Movie"
+  val ES_RATING_INDEX="Rating"
   val ES_TAG_INDEX="Tag"
   def main(args: Array[String]): Unit = {
 
@@ -98,7 +108,7 @@ object Dataloader {
     //将ratingRDD转换为DataFrame
     val ratingDF = ratingRDD.map(item => {
       val attr = item.split(",")
-      Rating(item(0).toInt, item(1).toInt, item(2).toDouble, item(3).toInt)
+      Rating(attr(0).toInt, attr(1).toInt, attr(2).toDouble, attr(3).toInt)
     }).toDF()
 
 
@@ -106,15 +116,44 @@ object Dataloader {
     //将tagRDD转换为DataFrame
     val tagDF = tagRDD.map(item => {
       val attr = item.split(",")
-      Tag(item(0).toInt, item(1).toInt, item(2).toString, item(3).toInt)
+      Tag(attr(0).toInt, attr(1).toInt, attr(2).toString, attr(3).toInt)
     }).toDF()
 
     implicit val mongoConfig = MongoConfig(config.get("mongo.uri").get,config.get("mongo.db").get)
 
     //将数据保存到mongodb
-    storeDataInMongoDB(movieDF,ratingDF,tagDF)
+    //storeDataInMongoDB(movieDF,ratingDF,tagDF)
+
+
+    //新的movie数据集(额外增加了tag字段)
+    //151^                                 电影ID
+    // Rob Roy (1995)^                     电影名称
+    // In the highlands of Scotlan         电影描述
+    // 139 minutes^                        电影时长
+    // August 26, 1997^                    电影发行时期
+    // 1995^                               电影拍摄日期
+    // English ^                           电影语言
+    // Action|Drama|Romance|War ^          电影类型
+    // Liam Neeson|Jess|m Neeson           电影演员
+    // Caton-Jones                         电影导演
+    // tag1/tag2/...                       电影的标签
+
+
+    //这里对tag数据集进行处理，处理后的形式为  MID   tag1/tag2/...
+    import org.apache.spark.sql.functions._
+    /**
+      * MID,Tags
+      * 1   tag1/tag2/...
+      */
+    val newTag = tagDF.groupBy($"mid").agg(concat_ws("|",collect_set($"tag")).as("tags"))
+    //需要将处理后的tag数据和movie数据融合，产生新的movie数据
+    val movieWithTagsDF = movieDF.join(newTag,Seq("mid","mid"),"left").select("mid","name","descri","timelong","issue","shoot","language","genres","actors","directors","tags")
+
+
+    implicit val esConfig = ESconfig(config.get("es.httpHosts").get,config.get("es.transportHosts").get,
+      config.get("es.index").get,config.get("es.cluster.name").get)
     //将数据保存到es
-    //storeDataInES()
+    storeDataInES(movieWithTagsDF)
 
     //关闭spark
     spark.stop()
@@ -169,7 +208,35 @@ object Dataloader {
   }
 
   //将数据保存到es的方法
-  def storeDataInES(): Unit = {
+  def storeDataInES(movieWithTagsDF:DataFrame)(implicit eSConfig: ESconfig): Unit = {
+    //新建一个配置
+    val settings:Settings = Settings.builder().put("cluster.name",eSConfig.clustername).build()
 
+    //新建ES客户端
+    val esClient = new PreBuiltTransportClient(settings)
+
+    //需要将transportHosts添加到esClient中
+    val REGEX_HOST_PORT = "(.+):(\\d+)".r
+    eSConfig.transportHosts.split(",").foreach{
+      case REGEX_HOST_PORT(host:String,port:String) =>{
+        esClient.addTransportAddress(new InetSocketTransportAddress(InetAddress.getByName(host),port.toInt))
+      }
+    }
+    //需要清除ES遗留的数据
+    if(esClient.admin().indices().exists(new IndicesExistsRequest(eSConfig.index)).actionGet().isExists){
+      esClient.admin().indices().delete(new DeleteIndexRequest(eSConfig.index))
+    }
+    esClient.admin().indices().create(new CreateIndexRequest(eSConfig.index))
+
+
+    //将数据写入到ES中
+    movieWithTagsDF
+      .write
+      .option("es.nodes",eSConfig.HttpHosts)
+      .option("es.http.timeout","100m")
+      .option("es.mapping.id","mid")
+      .mode("overwrite")
+      .format("org.elasticsearch.spark.sql")
+      .save(eSConfig.index+"/"+ES_MOVIE_INDEX)
   }
 }
