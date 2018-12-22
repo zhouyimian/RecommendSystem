f91fad16753119662334803d122f41e8ca7dbd0f
km <313976009@qq.com>
2018-12-22 16:19:41 +0800

    ALSTriner 对ALS模型的参数进行了测试，通过不同的均方根误差选择较优参数

---
+++ recommender/OfflineRecommender/src/main/scala/com/km/offline/ALSTriner.scala
@@ -0,0 +1,71 @@
+package com.km.offline
+
+import breeze.numerics.sqrt
+import org.apache.spark.SparkConf
+import org.apache.spark.mllib.recommendation.{ALS, MatrixFactorizationModel, Rating}
+import org.apache.spark.rdd.RDD
+import org.apache.spark.sql.SparkSession
+
+case class MongoConfig(val uri: String, val db: String)
+
+object ALSTriner {
+
+  def main(args: Array[String]): Unit = {
+    val config = Map(
+      "spark.cores" -> "local[*]",
+      "mongo.uri" -> "mongodb://RecommendSystem:27017/recommender",
+      "mongo.db" -> "recommender"
+    )
+
+
+    //创建SparkConf配置
+    val sparkConf = new SparkConf().setAppName("ALSTriner").setMaster(config("spark.cores"))
+    //创建一个Sparksession
+    val spark = SparkSession.builder().config(sparkConf).getOrCreate()
+
+    val mongoConfig = MongoConfig(config("mongo.uri"), config("mongo.db"))
+    import spark.implicits._
+    //加载评分数据
+    val ratingRDD = spark
+      .read
+      .option("uri", mongoConfig.uri)
+      .option("collection", OfflineRecommender.MONGODB_RATING_COLLECTION)
+      .format("com.mongodb.spark.sql")
+      .load()
+      .as[MovieRating]
+      .rdd
+      .map(rating => Rating(rating.uid, rating.mid, rating.score)).cache()
+
+    //输出最优参数
+    adjustASLParams(ratingRDD)
+
+    spark.close()
+  }
+  //输出最终的最优参数
+  def adjustASLParams(trainData:RDD[Rating]):Unit ={
+    val result =for (rank <- Array(30,40,50,60,70); lambda <- Array(1,0.1,0.01))
+      yield {
+        val model = ALS.train(trainData,rank,5,lambda)
+        val rmse = getRmse(model,trainData)
+        (rank,lambda,rmse)
+      }
+    println(result.sortBy(_._3).head)
+  }
+  def getRmse(model:MatrixFactorizationModel,trainData:RDD[Rating]):Double ={
+    val userMovies = trainData.map(item => (item.user,item.product))
+
+    val predictRating = model.predict(userMovies)
+
+    val real = trainData.map(item => ((item.user,item.product),item.rating))
+
+    val predict = predictRating.map(item => ((item.user,item.product),item.rating))
+
+    sqrt(
+      real.join(predict).map{
+        case ((uid,mid),(real,pre)) =>
+          val err = real-pre
+          err*err
+      }.mean()   //求均值
+    )
+  }
+}
